a51d2311a41749781e9cdc671a531b430e0cfe32
Yegor Bugayenko <yegor256@gmail.com>
2022-10-24 10:51:27 +0200

    -halt-on-error

--- build.lua
+++ build.lua
@@ -1,7 +1,7 @@
 module = "href-ul"
 ctanupload = true
-typesetopts = "--interaction=batchmode --shell-escape"
-checkopts = "--interaction=batchmode --shell-escape"
+typesetopts = "-interaction=batchmode -shell-escape -halt-on-error"
+checkopts = "-interaction=batchmode -shell-escape -halt-on-error"
 tagfiles = {"build.lua", "href-ul.dtx"}
 
 uploadconfig = {
