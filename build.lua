0c19aeb7ed62880acf1889fb367eeb8c827a0dad
rultor <me@rultor.com>
2024-11-01 13:07:11 +0000

    version set to 0.4.1

--- build.lua
+++ build.lua
@@ -28,7 +28,7 @@ tagfiles = {"build.lua", "href-ul.dtx"}
 
 uploadconfig = {
   pkg = "href-ul",
-  version = "0.0.0",
+  version = "0.4.1",
   author = "Yegor Bugayenko",
   uploader = "Yegor Bugayenko",
   email = "yegor256@gmail.com",
@@ -49,6 +49,6 @@ uploadconfig = {
 function update_tag(file, content, tagname, tagdate)
   return string.gsub(
     string.gsub(content, "0%.0%.0", tagname),
-    "0000/00/00", os.date("%Y/%m/%d")
+    "2024/11/01", os.date("%Y/%m/%d")
   )
 end
